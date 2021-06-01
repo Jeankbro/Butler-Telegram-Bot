93b04755598655f458ec8772bf0af3ef998560c2
Jeankbro <jeankbraham@gmail.com>
2021-06-01 13:07:13 -0500

    Funcionalidades del Bot
    El bot ya tiene sus 4 funciones básicas funcionando correctamente, sigue buscar la forma para que el bot esté siempre activo.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import json
 import telebot
 
+
 API_KEY = os.getenv("API_KEY")
 bot = telebot.TeleBot(API_KEY)
 
@@ -14,36 +15,34 @@ def get_mensaje_inspiracional():
     mensaje_final = "Aquí tengo una frase bonita, por ahora en inglés: \n\n" + mensaje
     return mensaje_final
 
-
 def kelvin_centigrados(temperatura):
-    return (temperatura - 273.15)
-
+  return (temperatura - 273.15)
 
 def get_clima():
-    API_KEY = os.getenv("API_KEY_WEATHER")
-    url_base = "http://api.openweathermap.org/data/2.5/weather?"
-    id_ciudad = "3672486"
-    url_completa = url_base + "id=" + id_ciudad + "appid=" + API_KEY
-    respuesta = requests.get(url_completa)
-    datos_json = respuesta.json()
-    if datos_json["cod"] != "404":
-        y = datos_json["main"]
-        temperatura = y["temp"]
-        temperatura_centi = kelvin_centigrados(temperatura)
-        return temperatura_centi
-
+  API_KEY = os.getenv("API_KEY_WEATHER")
+  url_base = "http://api.openweathermap.org/data/2.5/weather?"
+  id_ciudad = "3672486"
+  url_completa = url_base + "id=" + id_ciudad + "&appid=" + API_KEY
+  respuesta = requests.get(url_completa)
+  datos_json = respuesta.json()
+  if datos_json["cod"] != "404":
+    y = datos_json["main"]
+    temperatura = y["temp"]
+    temperatura_centi = kelvin_centigrados(temperatura)
+    return temperatura_centi
 
 def get_humedad():
-    API_KEY = os.getenv("API_KEY_WEATHER")
-    url_base = "http://api.openweathermap.org/data/2.5/weather?"
-    id_ciudad = "3672486"
-    url_completa = url_base + "id=" + id_ciudad + "appid=" + API_KEY
-    respuesta = requests.get(url_completa)
-    datos_json = respuesta.json()
-    if datos_json["cod"] != "404":
-        y = datos_json["main"]
-        humedad = y["humidity"]
-        return humedad
+  API_KEY = os.getenv("API_KEY_WEATHER")
+  url_base = "http://api.openweathermap.org/data/2.5/weather?"
+  id_ciudad = "3672486"
+  url_completa = url_base + "id=" + id_ciudad + "&appid=" + API_KEY
+  respuesta = requests.get(url_completa)
+  datos_json = respuesta.json()
+  if datos_json["cod"] != "404":
+    y = datos_json["main"]
+    humedad = y["humidity"]
+    return humedad
+  
 
 
 # Comandos del bot y sus respectivas acciones
@@ -56,22 +55,18 @@ def comandos(mensaje):
 /Inspírame Te doy una frase bonita :D
 /Clima Te doy el clima de hoy más la humedad!""")
 
-
 @bot.message_handler(commands=["Hola"])
 def saludo(mensaje):
-    bot.reply_to(mensaje, "Hola! como estás?")
+  bot.reply_to(mensaje, "Hola! como estás?")
 
 
 @bot.message_handler(commands=["Inspírame"])
 def inspirar(mensaje):
-    bot.reply_to(mensaje, get_mensaje_inspiracional())
-
+  bot.reply_to(mensaje, get_mensaje_inspiracional())
 
 @bot.message_handler(commands=["Clima"])
 def clima(mensaje):
-    bot.reply_to(mensaje,
-                 "Hoy estamos a unos " + str(get_clima()) + " grados centigrados, y con una humedad del " + str(
-                     get_humedad()) + "%!")
+  bot.reply_to(mensaje, "Hoy en Pereira estamos a unos " + str(get_clima()) + " grados centigrados, y con una humedad del " + str(get_humedad()) + "%!")
 
 
 bot.polling()
